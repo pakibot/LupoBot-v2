c43cca7b6cec4c0cc6b0fb062b3206b4f0fdfa63
0da0vi7d <50669284+0da0vi7d@users.noreply.github.com>
2021-05-02 10:34:50 +0200

    Command structure changed.

--- commands/play.js
+++ commands/play.js
@@ -3,6 +3,7 @@ const { Util, MessageEmbed } = require("discord.js");
 const ytdl = require("ytdl-core");
 const yts = require("yt-search");
 const sendError = require("../util/error");
+
 module.exports = {
     name: "play",
     async run (client, message, args) {
