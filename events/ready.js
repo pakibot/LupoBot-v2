524869d7e57b955cbb47091c9ce1e9f671441ff4
pakibot <78662789+pakibot@users.noreply.github.com>
2021-05-03 13:40:58 +0200

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,14 +1,14 @@
-module.exports = async (client) => {
-    console.log(`Online by @DavidCavallaro on GitHub`);
-    
-    let status = [
-        {name: `!help for info`, type: 'PLAYING'},
-        {name: `Online by @DavidCavallaro on GitHub`, type: 'PLAYING'},
+ module.exports = async (client) => {
+     console.log(`Online by @DavidCavallaro on GitHub`);
+ 
+   let status = [
+       {name: `l!help per i comandi`, type: 'PLAYING'},
+       {name: `online by Aboutpaki`, type: 'PLAYING'},
     ]
     function setStatus(){
-        let randomStatus = status[Math.floor(Math.random()*status.length)]
+      let randomStatus = status[Math.floor(Math.random()*status.length)]
         client.user.setPresence({activity: randomStatus})
     }
-    setStatus();
+    ''    setStatus();
     setInterval(() => setStatus(), 20000)
 };
